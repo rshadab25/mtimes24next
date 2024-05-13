b83a845ee2d1f0139626954e41f8a19a8bdf9857
Shadab Reza <rshadab25@gmail.com>
2024-05-13 09:08:52 +0530

    removed commented code

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -13,7 +13,7 @@ const RecentPosts = ({ blogs }) => {
         </h2>
         <Link
           href="/categories/all"
-          className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2      text-base md:text-lg"
+          className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2 text-base md:text-lg"
         >
           view all
         </Link>
