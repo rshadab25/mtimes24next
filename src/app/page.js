3b1b71caf4c35d4449119d5610788a29548e7364
Shadab Reza <rshadab25@gmail.com>
2024-05-26 12:28:28 +0530

    Remove caching from get api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 //import {allBlogs} from "contentlayer/generated";
 async function getPosts() {
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`, { next: { revalidate: 600 }}
+		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`, { cache: 'no-store' }
 	);
 	const posts = await response.json();
 	return posts;
